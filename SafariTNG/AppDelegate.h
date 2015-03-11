f491cd73677b32c761b86eb3a58f498417f0eb34
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-11 10:41:22 -0700

    Initial Commit

---
+++ SafariTNG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariTNG
+//
+//  Created by Ronald Hernandez on 3/11/15.
+//  Copyright (c) 2015 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
